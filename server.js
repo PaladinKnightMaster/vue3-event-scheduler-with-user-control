a54629a3047fa699054e18ae2126009c8f594526
Seren Martins <paladinknight.master@outlook.com>
2023-01-20 05:52:54 -0500

    skip cors

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
-const cors = require('cors');
+// const cors = require('cors');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-app.use(cors());
+// app.use(cors());
 const port = process.env.PORT || 3000;
 app.listen(port);
