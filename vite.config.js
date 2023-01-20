00d303607ec3f12fccb5a508808725c7358342cd
Seren Martins <paladinknight.master@outlook.com>
2023-01-19 21:51:59 -0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://water-calendar-website.herokuapp.com/",
+  // base: "https://water-calendar-website.herokuapp.com/",
   server: {
     port: 8080
   },
