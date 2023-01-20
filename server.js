afb562d9bd2cf8c3b7286946b8491574883a816e
topninja <michael.jinee@gmail.com>
2023-01-20 10:27:07 -0500

    fix

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const history = require('connect-history-api-fallback');
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 // const cors = require('cors');
 app = express();
+app.use(history());
 app.use(serveStatic(path.join(__dirname, 'dist')));
 // app.use(cors());
 const port = process.env.PORT || 3000;
