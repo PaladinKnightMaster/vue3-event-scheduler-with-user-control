9e44de97da89acd8ec4a4c3593027163c387c0e1
Seren Martins <paladinknight.master@outlook.com>
2023-01-19 21:43:11 -0500

    backend url fix

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://water-calendar-website.herokuapp.com/",
+  server: {
+    port: 8080
+  },
   css: {
     preprocessorOptions: {
         scss: {
