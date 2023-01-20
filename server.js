de2beed432c59e611a0bd4a32041c0f1627ff1cd
Seren Martins <paladinknight.master@outlook.com>
2023-01-20 02:02:54 -0500

    cors fix

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
+const cors = require('cors');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(cors());
 const port = process.env.PORT || 3000;
 app.listen(port);
